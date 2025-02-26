8540f8474b2646a7c77725a51b5a5c01bf0ae382
Katrein Hanna <katrein.hanna.96@gmail.com>
2025-02-26 23:29:18 +0300

    responsive design fixing

--- src/components/AboutusSection.jsx
+++ src/components/AboutusSection.jsx
@@ -1,25 +1,23 @@
 import AboutusCards from "./AboutusCards";
+import PageContent from "./PageContent";
 
 export default function AboutusSection() {
   return (
     <section className="main-container">
-      <div className="content-container flex flex-col justify-center items-center gap-10 mx-auto p-5 text-primary ">
-        <h1>Why Original Closets</h1>
-
-        <p>
-          Original Closets is a specialized carpentry company in Toronto with a
+      <PageContent
+        title="Why Original Closets"
+        description="Original Closets is a specialized carpentry company in Toronto with a
           focus on custom storage solutions for every room of your home or
           garage. We have more than 12 years experience in designing,
           manufacturing, and installing custom cabinets in your home or office
           at affordable prices. Our professionals will be pleased to offer
           creative ideas and services to assist you in designing your dream
-          rooms! Give us a try, we guarantee you’ll be pleased.
-        </p>
-        <p>
-          We proudly serve the Greater Toronto Area (GTA) including the City of
-          Toronto, Durham, Halton, Peel and York Regions.
-        </p>
-      </div>
+          rooms! Give us a try, we guarantee you’ll be pleased."
+      />
+      <PageContent
+        description="We proudly serve the Greater Toronto Area (GTA) including the City of
+          Toronto, Durham, Halton, Peel and York Regions."
+      />
       <AboutusCards />
     </section>
   );
--- src/components/PageContent.jsx
+++ src/components/PageContent.jsx
@@ -6,11 +6,11 @@ export default function PageContent({
 }) {
   return (
     <section className="main-container">
-      <div className="content-container items-start flex flex-col justify-center gap-5 mx-auto p-5 text-primary ">
+      <div className="content-container items-center flex flex-col justify-center gap-5 mx-auto px-2 text-primary ">
         {title == "" ? "" : <h1>{title}</h1>}
         {description == "" ? "" : <p className="text-start ">{description}</p>}
       </div>
-      <p className="uppercase text-primary/70 -mt-5 mb-8">{uppercaseDesc}</p>
+      <p className="uppercase text-primary/70 -mt-2 mb-8">{uppercaseDesc}</p>
     </section>
   );
 }
